7824604b7a335728d05c0d60a75dd1e49df263f8
Mariia2006 <145233153+Mariia2006@users.noreply.github.com>
2023-09-17 21:25:31 +0300

    Update Program5.cs

--- ConsoleApp8/Program5.cs
+++ ConsoleApp8/Program5.cs
@@ -1,8 +1,6 @@
 ﻿double x = 0;
 double y = 0;
-Console.WriteLine("Input x: ");
 x = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Input y: ");
 y = Convert.ToDouble(Console.ReadLine());
 double con = (Math.Pow(x, 2) / 9) + (Math.Pow(y, 2) / 1);
 if ((con <= 1 && y>=0) || (x >= -1 && x <= 1 && y <= 0 && y >= -2))
