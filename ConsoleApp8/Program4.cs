6f666744af91b240552439dda087095206b22f93
Mariia2006 <145233153+Mariia2006@users.noreply.github.com>
2023-09-17 15:11:40 +0300

    Add files via upload

---
+++ ConsoleApp8/Program4.cs
@@ -0,0 +1,4 @@
+﻿double x = 0;
+Console.WriteLine("Введiть х: ");
+x = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Обчислення 2^x*x*cos(x)+1: {Math.Pow(2, x) * x * Math.Cos(x) + 1}");
