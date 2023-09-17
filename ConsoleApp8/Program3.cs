69dbf0e73b8c628dc10f5c1847b7b7452581402f
Mariia2006 <145233153+Mariia2006@users.noreply.github.com>
2023-09-17 14:44:14 +0300

    Add files via upload

---
+++ ConsoleApp8/Program3.cs
@@ -0,0 +1,9 @@
+﻿double t = 0;
+double v = 0;
+Console.WriteLine("Введiть час (год): ");
+t = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введiть швидкiсть (км/год): ");
+v = Convert.ToDouble(Console.ReadLine());
+double s = t * v;
+Console.WriteLine($"Вiдстань дорiвнює (км): {s}");
+Console.ReadKey();
