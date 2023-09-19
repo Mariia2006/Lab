0b8e5a4c9f15b3cd6c70d80dccb5ffab01d14085
Mariia2006 <145233153+Mariia2006@users.noreply.github.com>
2023-09-19 21:37:21 +0300

    Update Program4.cs

--- ConsoleApp8/Program4.cs
+++ ConsoleApp8/Program4.cs
@@ -4,9 +4,8 @@ namespace Program
   class Program {
     static void Main (string[] args) {
       double x = 0;
-      Console.WriteLine("Введiть х: ");
       x = Convert.ToDouble(Console.ReadLine());
-      Console.WriteLine($"Обчислення 2^x*x*cos(x)+1: {Math.Pow(2, x) * x * Math.Cos(x) + 1}");
+      Console.WriteLine(Math.Pow(2, x) * x * Math.Cos(x) + 1);
     }
   }
 }
